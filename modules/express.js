59d8d016fa14017154a9b7a41237294b6ecbd9ce
PikaDude <mail@pikadude.me>
2019-07-25 21:59:49 +1000

    Finish

--- modules/express.js
+++ modules/express.js
@@ -6,51 +6,120 @@ var logger = require('morgan');
 var compression = require('compression');
 var helmet = require('helmet');
 var whiskers = require('whiskers');
+var bodyParser = require('body-parser');
 var request = require('requestretry');
 
 var app = express();
 app.engine('.html', whiskers.__express);
-app.set('views', './public');
+app.set('views', './public/views');
+app.use('/assets', express.static('public/assets'));
 app.use(logger(':remote-addr - :req[X-Forwarded-For] [:date[clf]] ":method :url HTTP/:http-version" :status ":referrer" ":user-agent"'));
 app.use(compression());
 app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(index.config.port);
 
 app.get('/', async function (req, res) {
-    if (req.query.token) {
-        res.render('create.html', {
-            TOKEN: req.query.token,
-            ERROR: req.query.error || ""
-        });
-    } else res.redirect(302, `https://discordapp.com/api/oauth2/authorize?client_id=${encodeURIComponent(index.config.discordClient.id)}&redirect_uri=${encodeURIComponent(index.config.discordClient.redirectURI)}&response_type=code&scope=identify%20guilds.join`);
+    res.redirect(302, `https://discordapp.com/api/oauth2/authorize?client_id=${encodeURIComponent(index.config.discordClient.id)}&redirect_uri=${encodeURIComponent(index.config.discordClient.redirectURI)}&response_type=code&scope=identify%20guilds.join`);
 });
 
-app.get('/api/create', async function (req, res) {
-    request({
-        url: 'https://discordapp.com/api/users/@me',
-        headers: {
-            'Authorization': 'Bearer ' + access_token
-        },
-        retryStrategy: myRetryStrategy,
-        delayStrategy: myDelayStrategy
-    }, function (err, response, body) {
-        if (err) {
-            res.status(500).send("Error communicating with Discord. Please try again later.");
-            console.error(err);
-        } else if (response.statusCode != 200) {
-            res.status(500).send("Error communicating with Discord. Please try again later.");
-            console.error(body);
-        } else {
-            try {
-                var userData = JSON.parse(body);
-
-                
-            } catch (Exception) {
-                res.status(500).send("An unknown error occurred. Please try again later.");
-                console.error(Exception);
-            }
-        }
+app.get('/create', async function (req, res) {
+    res.render('create.html', {
+        REGEX: index.config.account.allowedCharsRegex
     });
+});
+
+app.get('/complete', async function (req, res) {
+    res.render('complete.html', {
+        PLAY_HOST: index.config.playHost
+    });
+})
+
+app.post('/api/create', async function (req, res) {
+    if (req.body.token && req.body.name && req.body.password && req.body.color) {
+        if (!new RegExp(index.config.account.allowedCharsRegex).test(req.body.name) || req.body.name.length < 4 || req.body.name.length > 12) {
+            res.redirect(302, index.config.host + '/create?error=Invalid username.');
+            return;
+        }
+        if (req.body.password.length < 4 || req.body.password.length > 30) {
+            res.redirect(302, index.config.host + '/create?error=Invalid password.');
+            return;
+        }
+        if (req.body.color < 1 || req.body.color > 15 || req.body.color == 14) {
+            res.redirect(302, index.config.host + '/create?error=Invalid color.');
+            return;
+        }
+
+        var usernameChecks = await mysql.checkUsername(req.body.name);
+        if (usernameChecks.error) {
+            res.redirect(302, index.config.host + '/create?error=Error communicating with database. Please try again later.');
+            return;
+        } else if (usernameChecks.length > 0) {
+            res.redirect(302, index.config.host + '/create?error=Username is already taken.');
+            return;
+        }
+
+        request({
+            url: 'https://discordapp.com/api/users/@me',
+            headers: {
+                'Authorization': 'Bearer ' + req.body.token
+            },
+            retryStrategy: myRetryStrategy,
+            delayStrategy: myDelayStrategy
+        }, async function (err, response, body) {
+            if (err) {
+                res.redirect(302, index.config.host + '/create?error=Error communicating with Discord. Please try again later.');
+                console.error(err);
+            } else if (response.statusCode != 200) {
+                res.redirect(302, index.config.host + '/create?error=Error communicating with Discord. Please try again later.');
+                console.error(body);
+            } else {
+                try {
+                    var discordData = JSON.parse(body);
+
+                    if (index.config.discordJoin.enabled)
+                        request({
+                            url: `https://discordapp.com/api/v6/guilds/${index.config.discordJoin.serverID}/members/${discordData.id}`,
+                            method: 'PUT',
+                            json: {
+                                access_token: req.body.token
+                            },
+                            headers: {
+                                Authorization: 'Bot ' + index.config.discordJoin.botToken
+                            },
+                            retryStrategy: myRetryStrategy,
+                            delayStrategy: myDelayStrategy
+                        }, async function (err, response, body) {
+                            if (err) {
+                                console.error(err);
+                            } else if (response.statusCode != 204) {
+                                console.error(body);
+                            }
+
+                            var accountExists = await mysql.checkIfAccountUsed(discordData.id);
+                            if (accountExists.error) {
+                                res.redirect(302, index.config.host + '/create?error=Error communicating with database. Please try again later.');
+                                return;
+                            } else if (accountExists.length > 0) {
+                                res.redirect(302, index.config.host + '/create?error=A penguin is already associated with this Discord account.');
+                                return;
+                            }
+
+                            var insertedUser = await mysql.insertUser(req.body.name, req.body.password, req.body.color, discordData.id);
+                            if (insertedUser.error) {
+                                res.redirect(302, index.config.host + '/create?error=Error communicating with database. Please try again later.');
+                                return;
+                            } else {
+                                res.redirect(302, index.config.host + '/complete');
+                            }
+                        });
+                } catch (Exception) {
+                    res.redirect(302, index.config.host + '/create?error=Error communicating with database. Please try again later.');
+                    console.error(Exception);
+                }
+            }
+        });
+    } else res.status(400).send("Invalid body.");
 });
 
 app.get('/api/login', async function (req, res) {
@@ -80,7 +149,9 @@ app.get('/api/login', async function (req, res) {
             } else {
                 try {
                     var access_token = JSON.parse(body).access_token;
-                    res.redirect(302, index.config.host + '/?token=' + encodeURIComponent(access_token));
+                    res.render('saveToken.html', {
+                        TOKEN: access_token
+                    });
                 } catch (Exception) {
                     res.status(500).send("An unknown error occurred. Please try again later.");
                     console.error(Exception);
--- modules/mysql.js
+++ modules/mysql.js
@@ -1,13 +1,109 @@
 var index = require('../index.js');
 var mysql = require('mysql');
+var md5 = require('md5');
+var twinBcrypt = require('twin-bcrypt');
 
 var connection = mysql.createConnection(index.config.mysql);
 
 connection.connect();
 
-exports.insertUser = function (user) {
-    connection.query(`INSERT INTO discord_penguins (userID, penguinID) VALUES (${user.userID}, ${user.penguinID})`, function (error, results, fields) {
-        if (error) console.error(error);
-        console.log('The solution is: ', results[0].solution);
+exports.checkUsername = function (username) {
+    return new Promise(resolve => {
+        try {
+            connection.query(`SELECT Username FROM penguin WHERE Username = '${username}'`, function (error, results) {
+                if (error) {
+                    resolve({ error: error });
+                    console.error(error);
+                }
+                else resolve(results);
+            });
+        } catch (error) {
+            console.error(error);
+            resolve({ error: error });
+        }
     });
+}
+
+exports.checkIfAccountUsed = function (id) {
+    return new Promise(resolve => {
+        try {
+            connection.query(`SELECT userID FROM discord_penguins WHERE userID = '${id}'`, function (error, results) {
+                if (error) {
+                    resolve({ error: error });
+                    console.error(error);
+                }
+                else resolve(results);
+            });
+        } catch (error) {
+            console.error(error);
+            resolve({ error: error });
+        }
+    });
+}
+
+exports.insertUser = function (username, password, color, userID) {
+    return new Promise(resolve => {
+        try {
+            var hashedPassword = md5(password).toUpperCase();
+            var staticKey = "houdini";
+            var flashClientHash = getLoginHash(hashedPassword, staticKey);
+            twinBcrypt.hash(flashClientHash, 12, null, function (hash) {
+                connection.query(`INSERT INTO penguin (ID, Username, Nickname, Approval, Password, Email, Active, Color) VALUES (NULL, '${username}', '${username}', '1', '${hash}', '', '1', '${color}')`, function (error, results) {
+                    if (error) {
+                        resolve({ error: error });
+                        console.error(error);
+                    } else {
+                        connection.query(`INSERT INTO discord_penguins (userID, penguinID) VALUES ('${userID}', '${results.insertId}')`, function (error) {
+                            if (error) {
+                                resolve({ error: error });
+                                console.error(error);
+                            } else {
+                                connection.query(`INSERT INTO inventory (PenguinID, ItemID) VALUES ('${results.insertId}', '${color}')`, function (error) {
+                                    if (error) {
+                                        resolve({ error: error });
+                                        console.error(error);
+                                    } else {
+                                        connection.query(`INSERT INTO igloo (ID, PenguinID) VALUES (NULL, '${results.insertId}')`, function (error) {
+                                            if (error) {
+                                                resolve({ error: error });
+                                                console.error(error);
+                                            } else {
+                                                connection.query(`INSERT INTO postcard (ID, SenderID, RecipientID, Type) VALUES (NULL, NULL, '${results.insertId}', '125')`, function (error) {
+                                                    if (error) {
+                                                        resolve({ error: error });
+                                                        console.error(error);
+                                                    } else {
+                                                        resolve('Success');
+                                                    }
+                                                });
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            });
+        } catch (error) {
+            console.error(error);
+            resolve({ error: error });
+        }
+    });
+}
+
+function encryptPassword(password, MD5) {
+    if (MD5 != false) {
+        password = md5(password);
+    }
+    var hash = password.substr(16, 16) + password.substr(0, 16);
+    return hash;
+}
+
+function getLoginHash(password, staticKey) {
+    var hash = encryptPassword(password, false);
+    hash += staticKey;
+    hash += 'Y(02.>\'H}t":E1';
+    hash = encryptPassword(hash);
+    return hash;
 }
